210d8eff4c2211dda1d9e102dfa1f307d301bc99
zebawase <79371343+zebawase@users.noreply.github.com>
2022-04-27 14:19:14 +0530

    junittest

---
+++ src/TestPackage/JUnitFunctions.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class JUnitFunctions {
+	public int addnumber(int n1, int n2) {
+		return n1+n2;
+	}
+	public String addstring(String n1, String n2) {
+		return n1+n2;
+	}
+}
