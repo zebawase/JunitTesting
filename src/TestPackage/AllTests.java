4ee37f19870a9476bc65319bbfcc07b1018ba0d7
zebawase <79371343+zebawase@users.noreply.github.com>
2022-04-27 14:17:03 +0530

    junittest

---
+++ src/TestPackage/AllTests.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAddNumber.class, TestAddString.class })
+public class AllTests {
+
+}
